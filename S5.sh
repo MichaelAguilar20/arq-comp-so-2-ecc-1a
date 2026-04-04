f8faa208f79a334696479a581dbef340e83e75be
Michael Andrade <miaguilaran@uide.edu.ec>
2026-04-04 17:42:35 +0000

    wip: my first script

---
+++ S5.sh
@@ -0,0 +1,12 @@
+#lista archivos y directorios de la carpeta actual que estoy
+ls
+#muestra el contenido de un archivo en la terminal
+cat README.md
+#crea un directori bajo el directorio en el que estoy trabajndo
+mkdir padre1.txt
+#cambiamos el diectorio padre1.txt
+cd padre1.txt
+#creamos un archivo
+touch hijo1.txt
+#me muevo al directorio padre.txt del directorio actual
+cd ..
